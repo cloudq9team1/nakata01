deaaed284b3d72ab9a24bbba12dc9004b2714dce
nakata daiki <cloudq9team1@gmail.com>
2014-06-13 15:46:36 +0900

    upload

--- 06.mongodb.php/actions.php
+++ 06.mongodb.php/actions.php
@@ -1,6 +1,6 @@
 <?php
-$m = new MongoClient( 'mongodb://localhost:12345');
-$cursor = $m->tem->users->find( array( 'name' => 'marat'));
+$m = new MongoClient( 'mongodb://localhost:15000');
+$cursor = $m->tem->users->find( array( 'name' => 'nakata'));
 $result = $cursor->getNext();
 echo json_encode( $result); 
 ?>
